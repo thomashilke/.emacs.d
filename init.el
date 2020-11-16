d7df4711d71e68b0037098ec22853f5d1ed23561
Thomas Hilke <t.hilke@rollomatic.ch>
2020-11-16 09:52:32 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,11 @@
+(package-initialize)
+
+(require 'package)
+(setq package-archives '(("melpa" . "http://melpa.org/packages/") ("elpa" . "http://elpa.gnu.org/packages/")))
+
+(setq inhibit-startup-screen t)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(xterm-mouse-mode 1)
+
