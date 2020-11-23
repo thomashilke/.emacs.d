28e00210cf7254b75652de2b598176ccfdb196bc
Thomas Hilke <t.hilke@rollomatic.ch>
2020-11-23 09:24:22 +0100

    Tweak fsharp-mode and c++-mode, configure ripgrep call through counsel-mode

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (setq package-archives '(("melpa" . "http://melpa.org/packages/")
 			 ("elpa" . "http://elpa.gnu.org/packages/")))
 
-(setq required-packages-list '(company flycheck eglot magit projectile amx ivy counsel which-key yasnippet multiple-cursors ace-window delight fsharp-mode))
+(setq required-packages-list '(company flycheck eglot magit projectile amx ivy counsel which-key yasnippet multiple-cursors ace-window delight fsharp-mode yasnippet-snippets))
 
 (dolist (package required-packages-list)
   (when (not (package-installed-p package))
@@ -59,6 +59,9 @@
 
 (require 'counsel)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "M-s S") #'counsel-rg)
+(setq counsel-rg-base-command
+      "/usr/bin/rg -M 240 --with-filename --no-heading --line-number --color never %s --path-separator / .")
 
 (require 'swiper)
 (global-set-key (kbd "C-s") 'swiper-isearch)
@@ -106,15 +109,19 @@
 	    (flymake-mode 1)
 	    (setq flycheck-clang-language-standard "c++17")))
 
-(require 'cc-mode)
-(define-key c++-mode-map (kbd "C-c c") #'compile)
-
 (require 'flymake)
+
+(require 'cc-mode)
 (define-key c++-mode-map (kbd "C-c ! n") #'flymake-goto-next-error)
 (define-key c++-mode-map (kbd "C-c ! p") #'flymake-goto-prev-error)
 (define-key c++-mode-map (kbd "C-c ! l") #'flymake-show-diagnostics-buffer)
+(define-key c++-mode-map (kbd "C-c c") #'compile)
 
 (require 'fsharp-mode)
+(define-key fsharp-mode-map (kbd "C-c ! n") #'flymake-goto-next-error)
+(define-key fsharp-mode-map (kbd "C-c ! p") #'flymake-goto-prev-error)
+(define-key fsharp-mode-map (kbd "C-c ! l") #'flymake-show-diagnostics-buffer)
+
 (require 'eglot-fsharp)
 (add-hook 'fsharp-mode-hook #'eglot-ensure)
 (setq inferior-fsharp-program "/usr/bin/fsharpi --readline-")
@@ -162,3 +169,83 @@
       kept-old-versions 5    ; and how many of the old
       )
 
+;;; configure C++ indentation style
+(c-add-style "ThH"
+	     '("gnu"
+	       (c-basic-offset . 2)	; Guessed value
+	       (c-offsets-alist
+		(access-label . -)	; Guessed value
+		(block-close . 0)	; Guessed value
+		(class-close . 0)	; Guessed value
+		(class-open . 0)	; Guessed value
+		(defun-block-intro . +)	; Guessed value
+		(inclass . +)		; Guessed value
+		(inline-close . 0)	; Guessed value
+		(innamespace . +)	; Guessed value
+		(namespace-close . 0)	; Guessed value
+		(namespace-open . 0)	; Guessed value
+		(statement . 0)		    ; Guessed value
+		(statement-block-intro . +) ; Guessed value
+		(substatement-open . 0)	    ; Guessed value
+		(topmost-intro . 0)	    ; Guessed value
+		(annotation-top-cont . 0)
+		(annotation-var-cont . +)
+		(arglist-close . c-lineup-close-paren)
+		(arglist-cont c-lineup-gcc-asm-reg 0)
+		(arglist-cont-nonempty . c-lineup-arglist)
+		(arglist-intro . +)
+		(block-open . 0)
+		(brace-entry-open . 0)
+		(brace-list-close . 0)
+		(brace-list-entry . c-lineup-under-anchor)
+		(brace-list-intro . +)
+		(brace-list-open . 0)
+		(c . c-lineup-C-comments)
+		(case-label . 0)
+		(catch-clause . 0)
+		(comment-intro . c-lineup-comment)
+		(composition-close . 0)
+		(composition-open . 0)
+		(cpp-define-intro c-lineup-cpp-define +)
+		(cpp-macro . -1000)
+		(cpp-macro-cont . +)
+		(defun-close . 0)
+		(defun-open . 0)
+		(do-while-closure . 0)
+		(else-clause . 0)
+		(extern-lang-close . 0)
+		(extern-lang-open . 0)
+		(friend . 0)
+		(func-decl-cont . +)
+		(incomposition . +)
+		(inexpr-class . +)
+		(inexpr-statement . +)
+		(inextern-lang . +)
+		(inher-cont . c-lineup-multi-inher)
+		(inher-intro . +)
+		(inlambda . c-lineup-inexpr-block)
+		(inline-open . 0)
+		(inmodule . +)
+		(knr-argdecl . 0)
+		(knr-argdecl-intro . +)
+		(label . 2)
+		(lambda-intro-cont . +)
+		(member-init-cont . c-lineup-multi-inher)
+		(member-init-intro . +)
+		(module-close . 0)
+		(module-open . 0)
+		(objc-method-args-cont . c-lineup-ObjC-method-args)
+		(objc-method-call-cont c-lineup-ObjC-method-call-colons c-lineup-ObjC-method-call +)
+		(objc-method-intro .
+				   [0])
+		(statement-case-intro . +)
+		(statement-case-open . 0)
+		(statement-cont . +)
+		(stream-op . c-lineup-streamop)
+		(string . -1000)
+		(substatement . +)
+		(substatement-label . 2)
+		(template-args-cont c-lineup-template-args +)
+		(topmost-intro-cont . c-lineup-topmost-intro-cont))))
+
+(add-hook 'c++-mode-hook (lambda () (c-set-style "ThH")))
